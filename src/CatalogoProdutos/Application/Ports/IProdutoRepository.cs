5fa973a939ff54075f00ef8fb563a43c36516720
Gabriel <gabrielduquefernandes@gmail.com>
2026-03-30 15:55:26 -0300

    Criação das ports de produto e categorias

---
+++ src/CatalogoProdutos/Application/Ports/IProdutoRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+
+namespace Application.Ports
+{
+    internal interface IProdutoRepository
+    {
+       /// <summary>
+       /// Define os contratos para as operações do CRUD dos produtos
+       /// </summary>
+
+
+        //Portas para o GET dos produtos
+        Task<Produto?> GetProdutoByIdAsync(int id);
+        Task<IEnumerable<Produto>> GetProdutosByCategoria(int categoriaId);
+        Task<IEnumerable<Produto>> GetAllProdutos();
+
+
+
+        //Portas para o CREATE dos produtos
+        Task<Produto> CreateProdutoAsync(Produto produto);
+
+        
+        
+        //Portas para o UPDATE de produtos
+        Task<Produto> UpdateProdutoAsync(int id, Produto produtoAtualizado);
+
+
+
+        //Portas para o DELETE de produtos
+        Task<bool> DeleteProdutosAsync(int id);
+    }
+}
