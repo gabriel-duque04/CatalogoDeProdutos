47de6436e4d0a7b2b7951a4442e0b4dad225491c
Gabriel <gabrielduquefernandes@gmail.com>
2026-04-06 10:42:53 -0300

    Implementação do usecase de criar categoria

---
+++ src/CatalogoProdutos/Application/UseCases/Categorias/CreateCategoriaUseCase.cs
@@ -0,0 +1,30 @@
+﻿using Application;
+using Application.DTOs.Requests;
+using Application.Ports.PortsRepositories;
+using Application.Ports.PortsUseCases.Categorias;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Categorias
+{
+    public class CreateCategoriaUseCase : ICreateCategoriaUseCase
+    {
+        private readonly ICategoriaRepository _categoriaRepository;
+
+        public CreateCategoriaUseCase(ICategoriaRepository categoriaRepository)
+        {
+            _categoriaRepository = categoriaRepository;
+        }
+
+        public async Task<Categoria> ExecutarAsync(CategoriaRequestDTO categoria)
+        {
+            Categoria novaCategoria = new Categoria(categoria._nome, categoria._descricao);
+
+            return await _categoriaRepository.CreateCategoriaAsync(novaCategoria);
+        }
+    }
+}
