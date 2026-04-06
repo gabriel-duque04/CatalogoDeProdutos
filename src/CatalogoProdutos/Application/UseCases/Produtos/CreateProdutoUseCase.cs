449e7d17bb4ddc4f49b45e0a2b53470564cd2068
Gabriel <gabrielduquefernandes@gmail.com>
2026-04-06 16:13:31 -0300

    Implementação do usecase de criar produto

---
+++ src/CatalogoProdutos/Application/UseCases/Produtos/CreateProdutoUseCase.cs
@@ -0,0 +1,36 @@
+﻿using Application.DTOs.Requests;
+using Application.Ports.PortsRepositories;
+using Application.Ports.PortsUseCases.Produtos;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Produtos
+{
+    public class CreateProdutoUseCase : ICreateProdutoUseCase
+    {
+
+        private readonly IProdutoRepository _produtoRepository;
+
+        public CreateProdutoUseCase(IProdutoRepository produtoRepository)
+        {
+            _produtoRepository = produtoRepository;
+        }
+
+        public async Task<Produto> ExecutarAsync(ProdutoRequestDTO produto)
+        {
+            //Validações
+            if (String.IsNullOrEmpty(produto._nome))
+                throw new Exception("Nome é necessário;");
+
+            if (String.IsNullOrEmpty(produto._descricao))
+                throw new Exception("Descriçao é necessária");
+
+            Produto novòProduto = new Produto(produto._nome, produto._descricao, produto._preco, produto._categoriaId);
+
+        }
+    }
+}
