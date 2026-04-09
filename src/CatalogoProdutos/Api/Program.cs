eec3190818933e7b6c38b85fe23473762181f125
Gabriel <gabrielduquefernandes@gmail.com>
2026-04-09 16:19:58 -0300

    Configuração do swagger no projeto

--- src/CatalogoProdutos/Api/Program.cs
+++ src/CatalogoProdutos/Api/Program.cs
@@ -37,6 +37,14 @@ namespace Api
 
             
             //---------------------------------------------
+
+            //----------------------Swagger
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            //-----------------------------
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -45,16 +53,17 @@ namespace Api
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
 
             app.UseRouting();
 
             app.UseAuthorization();
 
-            app.MapRazorPages();
 
             app.Run();
         }
