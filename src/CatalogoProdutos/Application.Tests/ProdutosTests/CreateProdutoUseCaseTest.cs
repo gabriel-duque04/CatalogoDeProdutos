c585e315e10c22477310d4692f5fda89e056f4d3
Gabriel <gabrielduquefernandes@gmail.com>
2026-05-06 14:12:56 -0300

    Inicio do teste de criação de produtos

---
+++ src/CatalogoProdutos/Application.Tests/ProdutosTests/CreateProdutoUseCaseTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Application.UseCases.Produtos;
+using Application.Ports.PortsRepositories;
+using Domain.Entities;
+using Application.DTOs.Requests;
+using Xunit;
+
+namespace Application.Tests.ProdutosTests
+{
+    public class CreateProdutoUseCaseTest
+    {
+        private readonly Mock<IProdutoRepository> _produtoRepoMock;
+        private readonly Mock<ICategoriaRepository> _categoriaRepoMock;
+        private readonly CreateProdutoUseCase _useCase;
+
+        public CreateProdutoUseCaseTest()
+        {
+            this._produtoRepoMock = new Mock<IProdutoRepository>();
+            this._categoriaRepoMock = new Mock<ICategoriaRepository>();
+            this._useCase = new CreateProdutoUseCase(_produtoRepoMock.Object, _categoriaRepoMock.Object);
+        }
+
+
+    }
+}
