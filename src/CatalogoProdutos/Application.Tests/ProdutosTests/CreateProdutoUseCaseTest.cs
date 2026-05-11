88d406d96b59fc869fa7fa4d194aef911b7d5d1f
Gabriel <gabrielduquefernandes@gmail.com>
2026-05-11 15:10:57 -0300

    Criação do teste de update - produto não encontrado

--- src/CatalogoProdutos/Application.Tests/ProdutosTests/CreateProdutoUseCaseTest.cs
+++ src/CatalogoProdutos/Application.Tests/ProdutosTests/CreateProdutoUseCaseTest.cs
@@ -25,6 +25,39 @@ namespace Application.Tests.ProdutosTests
             this._useCase = new CreateProdutoUseCase(_produtoRepoMock.Object, _categoriaRepoMock.Object);
         }
 
+        [Fact]
+        public async Task ExecutarAsync_ExceptionQuandoCategoriaInexistente()
+        {
 
+            ProdutoRequestDTO request = new ProdutoRequestDTO("Pão de Queijo Congelado", "Um pão de queijo, só que congelado, esquentar antes de comer", 15, 99);
+            
+            _categoriaRepoMock.Setup(r => r.GetCategoriaByIdAsync(request.CategoriaID)).ReturnsAsync((Categoria)null);
+
+
+            var ex = await Assert.ThrowsAsync<Exception>(() => _useCase.ExecutarAsync(request));
+
+            Assert.Equal("Categoria não existente", ex.Message);
+
+            _produtoRepoMock.Verify(r => r.CreateProdutoAsync(It.IsAny<Produto>()), Times.Never);
+        }
+
+        [Fact]
+        public async Task ExecutarAsync_SalvarComSucesso_QuandoDadosValidados()
+        {
+            var request = new ProdutoRequestDTO("Pão de Queijo Congelado", "Um pão de queijo, só que congelado, esquentar antes de comer", 15, 99);
+
+            var categoriaExistente = new Categoria("Comida de mineiro", "A miô do mundo");
+
+            _categoriaRepoMock.Setup(r => r.GetCategoriaByIdAsync(request.CategoriaID))
+                              .ReturnsAsync(categoriaExistente);
+
+
+            await _useCase.ExecutarAsync(request);
+
+
+            _produtoRepoMock.Verify(r => r.CreateProdutoAsync(It.IsAny<Produto>()), Times.Once);
+
+
+        }
     }
 }
---
+++ src/CatalogoProdutos/Application.Tests/ProdutosTests/UpdateProdutoUseCaseTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Application.UseCases.Produtos;
+using Application.Ports.PortsRepositories;
+using Domain.Entities;
+using Application.DTOs.Requests;
+
+namespace Application.Tests.ProdutosTests
+{
+    public class UpdateProdutoUseCaseTest
+    {
+        private readonly Mock<IProdutoRepository> _produtoRepoMock;
+        private readonly Mock<ICategoriaRepository> _categoriaRepoMock;
+        private readonly UpdateProdutoUseCase _useCase;
+
+        public UpdateProdutoUseCaseTest()
+        {
+            _produtoRepoMock = new Mock<IProdutoRepository>();
+            _categoriaRepoMock = new Mock<ICategoriaRepository>();
+            _useCase = new UpdateProdutoUseCase(_produtoRepoMock.Object, _categoriaRepoMock.Object);
+        }
+
+        [Fact]
+        public async Task ExecutarAsync_ProdutoNaoExixte()
+        {
+            ProdutoRequestDTO request = new ProdutoRequestDTO("Editado", "teste edicao produto", 15, 99);
+            int idDoProduto = 50;
+
+            _produtoRepoMock.Setup(r => r.GetProdutoByIdAsync(idDoProduto))
+                            .ReturnsAsync((Produto)null);
+
+            var ex = await Assert.ThrowsAsync<Exception>(() => _useCase.ExecutarAsync(idDoProduto, request));
+            Assert.Equal("Produto inexistente", ex.Message);
+        }
+    }
+}
