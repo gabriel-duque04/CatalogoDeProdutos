4309437dc857c3209c1bfa3536f53e66bc02ffcd
Gabriel <gabrielduquefernandes@gmail.com>
2026-07-08 11:52:53 -0300

    feat: Criação das exceções para a categoria

---
+++ src/CatalogoProdutos/Application/Exceptions/Categorias/CategoriaNaoAtualizadaException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Exceptions.Categorias
+{
+    public class CategoriaNaoAtualizadaException : CatalogoException
+    {
+        public CategoriaNaoAtualizadaException(string message) : base(message)
+        {
+
+        }
+
+        public CategoriaNaoAtualizadaException() : base("Nâo foi possível atualizar a categoria, verifique os campos")
+        {
+
+        }
+    }
+}
---
+++ src/CatalogoProdutos/Application/Exceptions/Categorias/CategoriaNaoCriadaException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Exceptions.Categorias
+{
+    public class CategoriaNaoCriadaException : CatalogoException
+    {
+        public CategoriaNaoCriadaException(string message) : base(message)
+        {
+
+        }
+
+        public CategoriaNaoCriadaException() : base("Categoria não criada")
+        {
+
+        }
+    }
+}
