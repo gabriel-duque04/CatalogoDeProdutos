7b787b36fc81579a9c1aaa7248f27986e6c65f76
Gabriel <gabrielduquefernandes@gmail.com>
2026-07-08 10:44:07 -0300

    feat: Criação da classe base de exceções e duas exceeções personalizadas

---
+++ src/CatalogoProdutos/Application/Exceptions/CatalogoException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Exceptions
+{
+    public abstract class CatalogoException : Exception
+    {
+        protected CatalogoException(string mensagem) : base(mensagem)
+        {
+            
+        }
+    }
+}
---
+++ src/CatalogoProdutos/Application/Exceptions/CategoriaNaoEncontradaException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Exceptions
+{
+    public class CategoriaNaoEncontradaException : CatalogoException
+    {
+        public CategoriaNaoEncontradaException(string message) : base(message)
+        {
+
+        }
+
+        public CategoriaNaoEncontradaException() : base("A categoria não foi encontrada no sistema")
+        {
+
+        }
+    }
+}
